4d44d12f6d66a7d4d0628de0a558f6f6fd608a19
pedrobarco <pedro.barco@tecnico.ulisboa.pt>
2017-03-07 23:07:16 +0000

    updating embed messages, removed decode commando

--- commands/games/faceit.js
+++ commands/games/faceit.js
@@ -6,94 +6,90 @@ var rp = require('request-promise');
 var cheerio = require('cheerio');
 
 
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 
 class FaceItCommand extends commando.Command {
 
-    constructor(client) {
-        super(client, {
-            name: 'faceit',
-            group: 'games',
-            memberName: 'faceit',
-            description: 'Get a faceit user stats. Get your own stats by adding [-a] your username to database.',
-            examples: ['faceit [faceit-id]', 'faceit [-a] [faceit-id]', 'faceit'],
-        });
-    }
+  constructor(client) {
+    super(client, {
+      name: 'faceit',
+      group: 'games',
+      memberName: 'faceit',
+      description: 'Get a faceit user stats. Get your own stats by adding [-a] your username to database.',
+      examples: ['faceit [faceit-id]', 'faceit [-a] [faceit-id]', 'faceit'],
+    });
+  }
 
-    async run(message, args) {
-        var args_aux = args.split(" ");
-        var url = "http://faceitstats.com/profile,name,"
+  async run(message, args) {
+    var args_aux = args.split(" ");
+    var url = "http://faceitstats.com/profile,name,"
+    var new_faceitID = args_aux[1];
 
-
-        var new_faceitID = args_aux[1];
-        sql.get(`SELECT * FROM userdata WHERE userId ='${message.author.id}'`).then(row => {
-            if (!row) {
-                if (args_aux[0] == "-a") {
-                    sql.run('INSERT INTO userdata (userId, faceitID) VALUES (?, ?)', [message.author.id, new_faceitID]);
-                    message.reply("User added successfully. Type '!faceit' to see your stats.");
-                } else {
-                    message.reply("Database user not found. Use -a flag to set it.");
-                    return;
-                }
-            } else {
-                if (args_aux[0] == "-a") {
-                    row.faceitID = new_faceitID;
-                    message.reply("User updated successfully.");
-                } else if (args.length == 0) {
-                    getStats(row.faceitID);
-                } else {
-                    var faceitID = args_aux[0];
-                    getStats(faceitID);
-                }
-            }
-        }).catch(() => {
-            console.error;
-            sql.run('CREATE TABLE IF NOT EXISTS userdata (userId TEXT, faceitID TEXT)').then(() => {
-                sql.run('INSERT INTO userdata (userId, faceitID) VALUES (?, ?)', [message.author.id, new_faceitID]);
-                message.reply("User added successfully. Type '!faceit' to see your stats.");
-            });
-        });
-
-
-
-        function getStats(id) {
-            url += id;
-            var options = {
-                uri: url,
-                transform: function (body) {
-                    return cheerio.load(body);
-                }
-            };
-            rp(options)
-                .then(function ($) {
-                    var elo = $('.customh3 span').eq(0).text();
-                    var level = $('.customh3 span').eq(1).text();
-                    var rankup = $('.elo-container').prev().text();
-                    var stats = $('.boxStats').text();
-                    var avatar = $('#userView img').attr('src');
-                    rankup = rankup.replace(/you need/g, ":");
-                    var test = 'Level: ' + level + '\nElo: ' + elo;
-                    //message.channel.sendMessage(rank + "\n" + rankup + "\n" + stats);
-                    const embed = new Discord.RichEmbed()
-                        .setAuthor(id, 'https://files.catbox.moe/t0jwf4.jpg')
-                        .setColor(0xFF3517)
-                        .setURL(url)
-                        .addField('Stats', test)
-                        .setFooter('requested by ' + message.author.username, message.author.avatarURL)
-                    message.channel.sendEmbed(embed);
-                })
-            process.on('unhandledRejection', function (err) {
-                throw err;
-            });
-            process.on('uncaughtException', function (err) {
-                message.reply("That user does not have rank. Maybe you didn't type it correctly.");
-            });
+    sql.get(`SELECT * FROM userdata WHERE userId ='${message.author.id}'`).then(row => {
+      if (!row) {
+        if (args_aux[0] == "-a") {
+          sql.run('INSERT INTO userdata (userId, faceitID) VALUES (?, ?)', [message.author.id, new_faceitID]);
+          message.channel.sendMessage("User added successfully. Type '!faceit' to see your stats.");
+        } else {
+          message.channel.sendMessage("Database user not found. Use -a flag to set it.");
+          return;
         }
+      } else {
+        if (args_aux[0] == "-a") {
+          row.faceitID = new_faceitID;
+          message.channel.sendMessage("User updated successfully.");
+        } else if (args.length == 0) {
+          getStats(row.faceitID);
+        } else {
+          var faceitID = args_aux[0];
+          getStats(faceitID);
+        }
+      }
+    }).catch(() => {
+      console.error;
+      sql.run('CREATE TABLE IF NOT EXISTS userdata (userId TEXT, faceitID TEXT)').then(() => {
+        sql.run('INSERT INTO userdata (userId, faceitID) VALUES (?, ?)', [message.author.id, new_faceitID]);
+        message.channel.sendMessage("User added successfully. Type '!faceit' to see your stats.");
+      });
+    });
 
+    function getStats(id) {
+      url += id;
+      var options = {
+        uri: url,
+        transform: function (body) {
+          return cheerio.load(body);
+        }
+      };
+      rp(options)
+      .then(function ($) {
+        var elo = $('.customh3 span').eq(0).text();
+        var level = $('.customh3 span').eq(1).text();
+        var rankup = $('.elo-container').prev().text();
+        var stats = $('.boxStats').text();
+        var avatar = $('#userView img').attr('src');
+        rankup = rankup.replace(/you need/g, ":");
+        var alert = $('.alert').text();
+        var stats = 'Elo: ' + elo + '\nLevel: ' + level;
 
+        const embed = new Discord.RichEmbed()
+        .setAuthor(id, 'https://files.catbox.moe/t0jwf4.jpg')
+        .setColor(0xFF3517)
+        .setURL(url)
+        .addField('Stats', stats)
+        .setFooter('requested by ' + message.author.username, message.author.avatarURL)
+        message.channel.sendEmbed(embed);
 
+      })
+      process.on('unhandledRejection', function (err) {
+        throw err;
+      });
+      process.on('uncaughtException', function (err) {
+        message.channel.reply("Beep Boop! That user does not have rank. Maybe you didn't type it correctly.");
+      });
     }
+  }
 }
 
 module.exports = FaceItCommand;
--- commands/math/roll.js
+++ commands/math/roll.js
@@ -1,43 +1,43 @@
 const commando = require('discord.js-commando');
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 class RollCommand extends commando.Command {
 
-    constructor(client) {
-        super(client, {
-            name: 'roll',
-            group: 'math',
-            memberName: 'roll',
-            description: 'generates a random number between two limits',
-            examples: ['roll 0 100'],
-        });
+  constructor(client) {
+    super(client, {
+      name: 'roll',
+      group: 'math',
+      memberName: 'roll',
+      description: 'generates a random number between two limits or random word given a list',
+      examples: ['roll 0 100', 'roll foo bar 1 2 3'],
+    });
+  }
+
+  async run(message, args) {
+    var args_aux = args.split(" ");
+    var min = Math.round(args_aux[0]);
+    var max = Math.round(args_aux[1]);
+    var rng = Math.round(Math.random() * (max - min)) + min;
+    var string_rng = args_aux[Math.round(Math.random() * args_aux.length)];
+    var roll;
+
+    if (args_aux.length == 1) {
+      message.sendMessage("Something went wrong... Use '!help' command to know more about this.");
     }
-
-    async run(message, args) {
-        var args_aux = args.split(" ");
-
-        var min = Math.round(args_aux[0]);
-        var max = Math.round(args_aux[1]);
-        if (isNaN(min) || isNaN(max)) {
-            message.reply("the limits must be numbers")
-        }
-        else {
-            var roll = Math.round(Math.random() * (max - min)) + min;
-
-            if(args_aux.length == 2){
-                //message.reply("You rolled a " + roll);
-                    const embed = new Discord.RichEmbed()
-                        .setColor(0x3333BB)
-                        .addField('You rolled...', roll)
-                        .setFooter('requested by ' + message.author.username, message.author.avatarURL)
-                    message.channel.sendEmbed(embed);
-
-            } else {
-                message.reply("Something went wrong... Use '!help' command to know more about this.");
-            }
-        }
-
+    else {
+      if (args_aux.length == 2 && (!(isNaN(min) || isNaN(max)))) {
+        roll = rng;
+      }
+      else {
+        roll = string_rng;
+      }
+      const embed = new Discord.RichEmbed()
+      .setAuthor(roll, 'https://files.catbox.moe/z99ldc.png')
+      .setColor(0xb6f649)
+      .setFooter('requested by ' + message.author.username, message.author.avatarURL)
+      message.channel.sendEmbed(embed);
     }
+  }
 }
 
 module.exports = RollCommand;
